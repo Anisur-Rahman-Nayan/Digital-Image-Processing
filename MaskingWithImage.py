ea7aa795f7e9f639e816e8f9d2ed03142afabef2
Anisur Rahman Nayan <arnayan07sept@gmail.com>
2022-09-20 00:30:11 +0600

    Added Face Recognition Project in Python

--- MaskingWithImage.py
+++ MaskingWithImage.py
@@ -5,11 +5,13 @@ img = cv.imread("Resources/Photos/group 1.jpg")
 cv.imshow('group image',img)
 
 blank = np.zeros(img.shape[:2], dtype= 'uint8')
+
 circle = cv.circle(blank.copy(),(img.shape[1]//2, img.shape[0]//2),150,255,-1)
 rectangle = cv.rectangle(blank.copy(),(30,30),(370,370),255,-1)
 
 circle_masked = cv.bitwise_and(img,img,mask = circle)
 rectangle_masked = cv.bitwise_and(img,img,mask = rectangle)
+
 cv.imshow("Circle masked",circle_masked)
 cv.imshow("Rectangle masked", rectangle_masked)
 cv.waitKey(0)
